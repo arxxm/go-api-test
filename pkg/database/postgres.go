3a812dc30ef621a5ef77b64b4e8b54254ed407f7
arxxm <7767995@gmail.com>
2024-03-26 20:27:21 +0500

    feat: indexes

--- pkg/database/postgres.go
+++ pkg/database/postgres.go
@@ -26,5 +26,30 @@ func (p *Postgres) Init() (*pgx.Conn, error) {
 		return nil, err
 	}
 
+	_, err = conn.Exec(context.Background(), `
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            last_name VARCHAR(255) NOT NULL,
+            surname VARCHAR(255),
+            gender VARCHAR(20) NOT NULL,
+            status VARCHAR(20) NOT NULL,
+            date_of_birth TIMESTAMP,
+            created_at TIMESTAMP NOT NULL
+        );
+    `)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = conn.Exec(context.Background(), `
+        CREATE INDEX IF NOT EXISTS idx_user_id ON users (id);
+        CREATE INDEX IF NOT EXISTS idx_user_gender ON users (gender);
+        CREATE INDEX IF NOT EXISTS idx_user_status ON users (status);
+    `)
+	if err != nil {
+		return nil, err
+	}
+
 	return conn, nil
 }
